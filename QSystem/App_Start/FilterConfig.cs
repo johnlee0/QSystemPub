35c62ac4856604c5075869196af72f7e9b0128d6
Administrator <Administrator@DESKTOP-MF36D37>
2018-04-30 00:32:35 +0800

    添加项目文件。

---
+++ QSystem/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace QSystem
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ QSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QSystem.MvcApplication" Language="C#" %>
